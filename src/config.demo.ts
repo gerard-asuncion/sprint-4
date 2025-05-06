82b564c5085943f69a3bb6e33d45c90fe792cc50
GerardAsuncion <asunciongerard90@gmail.com>
2025-05-06 18:10:11 +0200

    fix: minor changes, config.demo.ts now has instructions

--- src/config.demo.ts
+++ src/config.demo.ts
@@ -1,1 +1,4 @@
-export const WEATHER_API_KEY = "your_key"
+// Copy and paste your key for "https://api.openweathermap.org/data/2.5/weather" API,
+// then change the name from config.demo.ts to config.ts and run npx tsc.
+
+export const WEATHER_API_KEY: string = "your_key_here"
